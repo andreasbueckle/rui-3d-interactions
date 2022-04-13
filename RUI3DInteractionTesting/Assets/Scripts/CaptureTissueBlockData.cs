fdba809253c9d9171f4df41084fbe1fbd8b769ca
andreasbueckle <abueckle@indiana.edu>
2022-04-13 10:19:24 -0400

    Update numbers

--- RUI3DInteractionTesting/Assets/Scripts/CaptureTissueBlockData.cs
+++ RUI3DInteractionTesting/Assets/Scripts/CaptureTissueBlockData.cs
@@ -6,8 +6,14 @@ public class CaptureTissueBlockData : MonoBehaviour
 {
     public delegate void Inventory(List<AnatomicalStructures> uniqueAS, List<CellTypes> uniqueCT);
     public static event Inventory UpdateInventoryEvent;
+<<<<<<< Updated upstream
     private List<AnatomicalStructures> m_UniqueAS = new List<AnatomicalStructures>();
     private List<CellTypes> m_UniqueCT = new List<CellTypes>();
+=======
+    private List<AnatomicalStructures> m_AnatomicalStructures = new List<AnatomicalStructures>();
+    private List<CellTypes> m_CellTypes = new List<CellTypes>();
+    [SerializeField] private int m_NumberTissueBlocks;
+>>>>>>> Stashed changes
 
 
     void OnEnable()
@@ -25,6 +31,12 @@ public class CaptureTissueBlockData : MonoBehaviour
         // Debug.Log(data.m_ListAS[0]);
         UpdateAS(data.m_ListAS, isCollided);
         UpdateCT(data.m_ListCT, isCollided);
+<<<<<<< Updated upstream
+=======
+        UpdateNumberTissueBlock(isCollided);
+        UpdateInventoryEvent?.Invoke(m_AnatomicalStructures, m_CellTypes, m_NumberTissueBlocks);
+    }
+>>>>>>> Stashed changes
 
         UpdateInventoryEvent?.Invoke(m_UniqueAS, m_UniqueCT);
     }
@@ -35,7 +47,7 @@ public class CaptureTissueBlockData : MonoBehaviour
         {
             foreach (var item in list)
             {
-                m_UniqueAS.Add(item);
+                m_AnatomicalStructures.Add(item);
             }
 
         }
@@ -43,18 +55,18 @@ public class CaptureTissueBlockData : MonoBehaviour
         {
             foreach (var item in list)
             {
-                m_UniqueAS.Remove(item);
+                m_AnatomicalStructures.Remove(item);
             }
         }
-
     }
+
     void UpdateCT(List<CellTypes> list, bool isCollided)
     {
         if (isCollided)
         {
             foreach (var item in list)
             {
-                m_UniqueCT.Add(item);
+                m_CellTypes.Add(item);
             }
 
         }
@@ -62,7 +74,7 @@ public class CaptureTissueBlockData : MonoBehaviour
         {
             foreach (var item in list)
             {
-                m_UniqueCT.Remove(item);
+                m_CellTypes.Remove(item);
             }
         }
     }
